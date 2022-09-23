4e8a94d9b6dff9d7ff05076dcd5f8f0f2aedd429
a1k0u <alko7enko@gmail.com>
2022-09-23 18:15:20 +0300

    refactor and add fourth task

--- HW05.hs
+++ HW05.hs
@@ -1,21 +1,11 @@
--- Practice 4
-evenCount [] = 0
-evenCount (x : xs) | even x    = 1 + evenCount xs
-                   | otherwise = evenCount xs
+{-
+    Sums three array by each element.
 
-onlyOdds [] = []
-onlyOdds (x : xs) | odd x     = x : onlyOdds xs
-                  | otherwise = onlyOdds xs
-permuteOddsAndEvens [x] = [x]
-permuteOddsAndEvens []  = []
-permuteOddsAndEvens (x : y : xs)
-    | odd x && even y = y : x : permuteOddsAndEvens xs
-    | even x && odd y = y : x : permuteOddsAndEvens xs
-    | otherwise       = x : y : permuteOddsAndEvens xs
+    Create all state for each array,
+    it's like enumeration of all conditions.
+-}
 
-
--- Sums arrays by element
-
+sum3 :: [Integer] -> [Integer] -> [Integer] -> [Integer]
 sum3 []       []       []       = []
 
 sum3 (x : xs) []       []       = (x + 0 + 0) : sum3 xs [] []
@@ -28,7 +18,13 @@ sum3 (x : xs) []       (z : zs) = (x + 0 + z) : sum3 xs [] zs
 
 sum3 (x : xs) (y : ys) (z : zs) = (x + y + z) : sum3 xs ys zs
 
--- Split digits
+
+{-
+    Convert number into array of digits, similar to split number.
+
+    Firstly convert number from negative to positive,
+    then split it by recursion.
+-}
 
 goToPositive :: Integer -> Integer
 goToPositive n | n < 0     = n * (-1)
@@ -43,29 +39,33 @@ digits n = splitDigits num where num = goToPositive n
 
 -- Containg
 
-containDigit [] d = False
-containDigit (n : ns) d | n == d    = True
-                        | otherwise = containDigit ns d
+{-
 
+-}
+
+
+countDigits :: [Integer] -> Integer -> Integer
+countDigits [] d = 0
+countDigits (n : ns) d
+    | n == d    = 1 + countDigits ns d
+    | otherwise = countDigits ns d
+
+convertIntBool :: Integer -> (Integer -> Bool) -> Bool
+convertIntBool num func
+    | func num  = True
+    | otherwise = False
+
+containsAllHelper :: [Integer] -> Integer -> (Integer -> Bool) -> Bool
+containsAllHelper digits counter func
+    | counter == 10 = True
+    | otherwise     =
+        convertIntBool (countDigits digits counter) func &&
+            containsAllHelper digits (counter + 1) func
 
 containsAllDigits :: Integer -> Bool
-containsAllDigits n
-    | containDigit d 1
-        && containDigit d 2
-        && containDigit d 3
-        && containDigit d 4
-        && containDigit d 5
-        && containDigit d 6
-        && containDigit d 7
-        && containDigit d 8
-        && containDigit d 9
-    = True
-    | otherwise
-    = False
-    where d = digits n
-
-
-
+containsAllDigits n = containsAllHelper d 1 (0 /=) where d = digits n
 
 -- Contains all digits ones
 
+containsAllDigitsOnes :: Integer -> Bool
+containsAllDigitsOnes n = containsAllHelper d 1 (1 ==) where d = digits n
